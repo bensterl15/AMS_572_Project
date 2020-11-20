7a710f861cd87aedda92e595e3a5f95a47e84537
bsterling <bsterling@sterlingmedicaldevices.com>
2020-11-20 17:23:50 -0500

    Last few commits

--- data/Hypothesis_1.R
+++ data/Hypothesis_1.R
@@ -1,62 +1,58 @@
-data_Redbluff = read.csv("Redbluff_R_Formatted.csv");
-data_roseburg = read.csv("roseburg.csv");
-#data_sweetfield = read.csv("sweet_field_oregon.csv");
-data_sweetfield = read.csv("vegas.csv");
+# High Elevation:
+data_salt_lake_city = read.csv("salt_lake_city.csv"); #1287.8m elevation
+data_lovelock = read.csv("lovelock_derby.csv"); #1189.3m elevation
 
-Redbluff_Temp_Data = data_Redbluff$Avg..Annual.Temp;
-roseburg_Temp_Data = data_roseburg$TMAX;
+#Low Elevation:
+data_Redbluff = read.csv("Redbluff_R_Formatted.csv"); #93.0m elevation
+data_orlando = read.csv("orlando.csv"); #32.9m elevation
+data_sweetfield = read.csv("sweet_field_oregon.csv"); # 107.6m elevation
+
+salt_lake_Temp_Data = data_salt_lake_city$TMAX;
+
+Redbluff_Temp_Data = data_Redbluff$Avg..Annual.Max.Temp;
+orlando_Temp_Data = data_orlando$TMAX;
 sweetfield_Temp_Data = data_sweetfield$TMAX;
+lovelock_Temp_Data = data_lovelock$TMAX;
 
-Redbluff_Diff = NA;
+# Calculate means: (must round to first decimal place because this is the precision of our data:
+salt_lake_mean = round(10*mean(salt_lake_Temp_Data[1:26],na.rm=TRUE))/10;
+lovelock_mean = round(10*mean(lovelock_Temp_Data[1:10],na.rm=TRUE))/10;
 
-for(i in 1:(length(Redbluff_Temp_Data) - 1)){
-	Redbluff_Diff = c(Redbluff_Diff, Redbluff_Temp_Data[i+1] - Redbluff_Temp_Data[i]);
-}
 
-roseburg_Diff = NA;
+Redbluff_mean = round(10*mean(Redbluff_Temp_Data[1:49],na.rm=TRUE))/10;
+orlando_mean = round(10*mean(orlando_Temp_Data[1:19],na.rm=TRUE))/10;
+sweetfield_mean = round(10*mean(sweetfield_Temp_Data[1:26],na.rm=TRUE))/10;
 
-for(i in 1:(length(roseburg_Temp_Data) - 1)){
-	roseburg_Diff = c(roseburg_Diff, roseburg_Temp_Data[i+1] - roseburg_Temp_Data[i]);
-}
 
-sweetfield_Diff = NA;
+salt_lake_Diff = salt_lake_Temp_Data[27:length(salt_lake_Temp_Data)] - salt_lake_mean;
+Redbluff_Diff = Redbluff_Temp_Data[50:length(Redbluff_Temp_Data)] - Redbluff_mean;
+orlando_Diff = orlando_Temp_Data[1:length(orlando_Temp_Data)] - orlando_mean;
+sweetfield_Diff = sweetfield_Temp_Data[27:length(sweetfield_Temp_Data)] - sweetfield_mean;
+lovelock_Diff = lovelock_Temp_Data[11:length(lovelock_Temp_Data)] - lovelock_mean;
 
-for(i in 1:(length(sweetfield_Temp_Data) - 1)){
-	sweetfield_Diff = c(sweetfield_Diff, sweetfield_Temp_Data[i+1] - sweetfield_Temp_Data[i]);
-}
-
-Redbluff_Diff = Redbluff_Diff[79:length(Redbluff_Diff)]
-roseburg_Diff = roseburg_Diff[40:length(roseburg_Diff)]
-
+salt_lake_Diff = salt_lake_Diff[!is.na(salt_lake_Diff)];
 Redbluff_Diff = Redbluff_Diff[!is.na(Redbluff_Diff)];
-roseburg_Diff = roseburg_Diff[!is.na(roseburg_Diff)];
+orlando_Diff = orlando_Diff[!is.na(orlando_Diff)];
 sweetfield_Diff = sweetfield_Diff[!is.na(sweetfield_Diff)];
+lovelock_Diff = lovelock_Diff[!is.na(lovelock_Diff)];
 
-#print(Redbluff_Diff);
-#shapiro.test(Redbluff_Diff);
+#hist(salt_lake_Diff);
+#hist(lovelock_Diff, breaks = seq(-5,5,0.5));
 
-#print(roseburg_Diff);
-#shapiro.test(roseburg_Diff);
+print(lovelock_Diff)
 
-print(var(Redbluff_Diff));
-print(mean(Redbluff_Diff));
+# Create vector of low elevation temperature deviations:
+low_elevation_diffs = c(Redbluff_Diff, orlando_Diff, sweetfield_Diff);
 
-shapiro.test(Redbluff_Diff);
+# Create vector of high elevation temperature deviations:
+high_elevation_diffs = c(salt_lake_Diff, lovelock_Diff);
 
-#low_elevation_diffs = c(Redbluff_Diff, roseburg_Diff, sweetfield_Diff)
-
-low_elevation_diffs = c(roseburg_Diff, sweetfield_Diff)
-
-print(var(low_elevation_diffs));
-print(mean(low_elevation_diffs));
-
+shapiro.test(salt_lake_Diff);
+shapiro.test(lovelock_Diff);
+shapiro.test(high_elevation_diffs);
 #shapiro.test(low_elevation_diffs);
 
-t.test(Redbluff_Diff);
-
-t.test(roseburg_Diff);
-
-t.test(sweetfield_Diff);
+t.test(salt_lake_Diff);
 
 t.test(low_elevation_diffs);
 
@@ -64,6 +60,6 @@ t.test(low_elevation_diffs);
 #shapiro.test(sweetfield_Diff);
 
 #Redbluff_Diff = Redbluff_Diff[!is.na(Redbluff_Diff)];
-#roseburg_Diff = roseburg_Diff[!is.na(roseburg_Diff)];
+#orlando_Diff = orlando_Diff[!is.na(orlando_Diff)];
 
-#t.test(Redbluff_Diff, roseburg_Diff);
+#t.test(Redbluff_Diff, orlando_Diff);
