ebe3da041fc152cb022c8eb78c94735dd87f77bf
bsterling <bsterling@sterlingmedicaldevices.com>
2020-11-27 16:57:26 -0500

    Figures

--- data/Hypothesis_1.R
+++ data/Hypothesis_1.R
@@ -6,7 +6,7 @@ library(latex2exp);
 pdf(NULL);
 
 data_Salt_Lake = read.csv("GSOY/GSOM_Salt_Lake.csv"); #1287.8m elevation
-data_rye_patch = read.csv("GSOY/GSOM_rye_patch_updated dates.csv"); #1260.3m elevation
+data_rye_patch = read.csv("GSOY/GSOM_rye_patch_updated dates_monthly adjustment.csv"); #1260.3m elevation
 #data_rye_patch = read.csv("GSOY/GSOM_rye_patch.csv"); #1260.3m elevation
 
 Salt_Lake_Temp_Data = data_Salt_Lake$TAVG;
@@ -18,6 +18,8 @@ rye_patch_mean = round(10 * mean(rye_patch_Temp_Data[1:264], na.rm=TRUE)) / 10;
 
 Salt_Lake_Diff = Salt_Lake_Temp_Data[265:length(Salt_Lake_Temp_Data)] - Salt_Lake_mean;
 rye_patch_Diff = rye_patch_Temp_Data[265:length(rye_patch_Temp_Data)] - rye_patch_mean;
+
+rye_patch_Diff = (data_rye_patch$TAVG.ADJ)[265:length(data_rye_patch$TAVG.ADJ)];
 
 
 for(i  in 1:length(Salt_Lake_Diff)){
@@ -54,7 +56,7 @@ print("rye_patch");
 #print(paste("\U03BC", "=", mean(rye_patch_Diff, na.rm=TRUE)));
 #print(paste("\U03C3", "=", sqrt(var(rye_patch_Diff, na.rm=TRUE))));
 print(paste("mean =", mean(rye_patch_Diff, na.rm=TRUE)));
-print(paste("mean =", sqrt(var(rye_patch_Diff, na.rm=TRUE))));
+print(paste("std =", sqrt(var(rye_patch_Diff, na.rm=TRUE))));
 print(paste("n =",length(rye_patch_Diff)));
 # New line:
 cat("\n");
@@ -78,4 +80,4 @@ abline(a = min(rye_patch_Diff), b = (max(rye_patch_Diff) - min(rye_patch_Diff)) 
 dev.off();
 
 #Perform two sided t-test with unequal variances:
-t.test(rye_patch_Diff, Salt_Lake_Diff, paired=TRUE);
+t.test(rye_patch_Diff, Salt_Lake_Diff, paired=FALSE);
